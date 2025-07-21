021b52f316844a812cafb461444e6b3d10e50ba5
smoleck <kacper.smolkowski@gmail.com>
2025-07-21 21:25:52 +0200

    innit

---
+++ src/main/java/com/example/sq_ch11_ex1/SqCh11Ex1Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch11_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh11Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh11Ex1Application.class, args);
+	}
+
+}
